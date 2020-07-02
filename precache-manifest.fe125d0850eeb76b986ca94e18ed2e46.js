07a66adad3ea17a146c4fe4727477910dc645c6c
Rana Emad <rana3emad@gmail.com>
2020-07-02 19:35:13 +0200

    Updates

---
+++ precache-manifest.fe125d0850eeb76b986ca94e18ed2e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d26d5fb45056d167ef1c121b7ad3cd4",
+    "url": "/whiteboard/index.html"
+  },
+  {
+    "revision": "0cb6f1bb70e5277fa7ae",
+    "url": "/whiteboard/static/css/main.a1c13837.chunk.css"
+  },
+  {
+    "revision": "ae42112d27d400c517a4",
+    "url": "/whiteboard/static/js/2.6b73f32b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whiteboard/static/js/2.6b73f32b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb6f1bb70e5277fa7ae",
+    "url": "/whiteboard/static/js/main.a8f77be0.chunk.js"
+  },
+  {
+    "revision": "95f52f808f7b1ca668e1",
+    "url": "/whiteboard/static/js/runtime-main.c2696591.js"
+  }
+]);
