8f2f5e2daef5b62927498a4231485c6f9f33c12b
Rana Emad <rana3emad@gmail.com>
2020-07-02 16:23:39 +0200

    board component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Board from "./components/Board/Board";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Board/Board.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import "./Board.css";
+
+function Board() {
+  const canvasRef = React.useRef(null);
+  const parentRef = React.useRef(null);
+  const [ctx, setCtx] = useState({});
+  const [canvasOffset, setCanvasOffset] = useState({ x: 0, y: 0 });
+  const [drawing, setDrawing] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    let canv = canvasRef.current;
+    canv.width = parentRef.current.offsetWidth;
+    canv.height = parentRef.current.offsetHeight;
+
+    let canvCtx = canv.getContext("2d");
+    canvCtx.lineJoin = "round";
+    canvCtx.lineCap = "round";
+    canvCtx.lineWidth = 5;
+    setCtx(canvCtx);
+
+    let offset = canv.getBoundingClientRect();
+    setCanvasOffset({ x: parseInt(offset.left), y: parseInt(offset.top) });
+  }, [ctx]);
+
+  function handleMouseDown(e) {
+    setDrawing(true);
+    setPosition({
+        x: parseInt(e.clientX - canvasOffset.x),
+        y: parseInt(e.clientY - canvasOffset.y),
+      });
+  }
+  function handleMouseUp() {
+    setDrawing(false);
+  }
+
+  function handleMouseMove(e) {
+    let mousex = e.clientX - canvasOffset.x;
+    let mousey = e.clientY - canvasOffset.y;
+    if (drawing) {
+      ctx.strokeStyle = "#000000";
+      ctx.beginPath();
+      ctx.moveTo(position.x, position.y);
+      ctx.lineTo(mousex, mousey);
+      ctx.stroke();
+    }
+    setPosition({ x: mousex, y: mousey });
+  }
+
+  return (
+    <div className="board" ref={parentRef}>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      />
+    </div>
+  );
+}
+
+export default Board;
